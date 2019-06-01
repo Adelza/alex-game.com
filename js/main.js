4caa90bfca8f0b9b29cbc5e4b02832aac9cf9a0c
Adelza <adelza@email.ua>
2019-06-01 20:16:02 +0300

    1

--- js/main.js
+++ js/main.js
@@ -52,3 +52,18 @@ document.addEventListener('DOMContentLoaded', function (e) {
     tf.init();
 });
 
+$(document).ready(function (){
+if(ontouchstart in window) { //проверить, является ли экран touch-screen'ом
+$('game-icon-img').click(function(e){ //клик по элементу с иконкой игры
+e.preventDefault(); //отменить стандартный переход по ссылке
+$('game-icon-text').removeClass('show'); //убрать класс .show у ВСЕХ элементов с кнопками играть и правила
+$(this).find('game-icon-text').addClass('show'); //добавить класс .show данному элементу с кнопками играть и правила
+});
+}
+
+
+
+    $(document).ready(function(){
+alert('Ваша версия jQuery ' + jQuery.fn.jquery);
+});
+
